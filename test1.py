cedbe9e5f4a2e0e9ab672a4a64cbfb115ff4e3ca
Klaus <k.breuer.com@googlemail.com>
2017-06-13 17:02:26 +0200

    Django Girls Tutorial, initial commit.

---
+++ test1.py
@@ -0,0 +1,12 @@
+girls = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'You']
+
+
+def hi(name):
+    print('Hi ' + name + '!')
+
+
+for name in girls:
+    hi(name)
+
+for i in range(1, 20, 3):
+    print(i)
